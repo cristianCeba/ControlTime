191e8662e681ace12deac69915424e0fe2aa8dcb
cristianCeba <cceballosocana@educa.madrid.org.com>
2021-09-23 19:57:26 +0200

    actividad de fichaje modificada

--- app/src/main/java/com/example/controltime/Activity_fichaje.java
+++ app/src/main/java/com/example/controltime/Activity_fichaje.java
@@ -1,5 +1,6 @@
 package com.example.controltime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -8,12 +9,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.EventListener;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,6 +33,9 @@ public class Activity_fichaje extends AppCompatActivity {
     TextView textMostHora, textMostDia;
     private DatabaseReference mDataBase;
     Date hora;
+    String dia;
+    String usuarioAplicacion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,21 +46,49 @@ public class Activity_fichaje extends AppCompatActivity {
         btnFinFichaje = findViewById(R.id.btnFinFichaje);
         textMostHora = findViewById(R.id.textHora);
         textMostDia = findViewById(R.id.textDia);
+        usuarioAplicacion = User.UsuarioConectadoApp(getApplicationContext()).replace(".", "_").trim();
         //Fecha que usamos para guardar en la base de datos ejemplo --> 22:09:2021
-        String date = new SimpleDateFormat("dd:MM:yyyy").format(new Date());
+        dia = new SimpleDateFormat("dd:MM:yyyy").format(new Date());
 
 
+        //Recuperamos los datos del usuario del día.
+        Query query=mDataBase.child("fichaje").child(usuarioAplicacion);
+        System.out.println("Usuario --> " +
+                usuarioAplicacion);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                 @Override
+                                                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                                     for(DataSnapshot ds: snapshot.getChildren()){
+                                                         Fichaje fichajeUsuario = ds.getValue(Fichaje.class);
+                                                         System.out.println("ds.getKey() --> " + ds.getKey());
+                                                         System.out.println("dia --> " + dia);
+                                                         if (ds.getKey().equals(dia)){
+                                                             btnIniFichaje.setEnabled(false);
+                                                         }
+                                                     }
+
+
+
+                                                 }
+
+                                                 @Override
+                                                 public void onCancelled(@NonNull DatabaseError error) {
+
+                                                 }
+                                             });
 
         // textView is the TextView view that should display it
-        textMostDia.setText(date);
+
+        textMostDia.setText(dia);
 
         btnIniFichaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Fecha que usamos para guardar en la base de datos ejemplo --> 22:09:2021
-                String date = new SimpleDateFormat("dd:MM:yyyy").format(new Date());
                 Fichaje fichaje = new Fichaje (textMostHora.getText().toString());
-                mDataBase.child("fichaje").child("1").child(date).setValue(fichaje);
+                mDataBase.child("fichaje").child(usuarioAplicacion).child(dia).setValue(fichaje);
+                btnIniFichaje.setEnabled(false);
             }
         });
 
@@ -76,14 +113,10 @@ public class Activity_fichaje extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    System.out.println("1");
                     hora = new Date();
-                    System.out.println("2");
                     String h = dateFormat.format(hora);
-                    System.out.println("3");
                     textMostHora.setText("");
                     textMostHora.setText(h);
-                    System.out.println("4");
                 }catch (Exception e){
                     System.out.println("error");
                     this.run();
--- app/src/main/java/com/example/controltime/Fichaje.java
+++ app/src/main/java/com/example/controltime/Fichaje.java
@@ -3,18 +3,22 @@ package com.example.controltime;
 public class Fichaje {
 
     public String horaIni;
-    public String horaFin;
-    public String horaIniDescanso;
-    public String horaFinDescanso;
-
+   // public String horaFin;
+   // public String horaIniDescanso;
+   // public String horaFinDescanso;
+/*
     public Fichaje (String horaInicio, String horaFinal, String horaInicioDescanso, String horaFinalDescanso){
         this.horaIni = horaInicio;
         this.horaFin = horaFinal;
         this.horaIniDescanso  = horaInicioDescanso;
         this.horaFinDescanso = horaFinalDescanso;
-    }
+    }*/
 
     public Fichaje (String horaInicio){
         this.horaIni = horaInicio;
+    }
+
+    public Fichaje (){
+
     }
 }
